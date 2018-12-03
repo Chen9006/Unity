24f60a424a14ba277d9c9fcca40d74cfaeab378a
cjq1997 <408715746@qq.com>
2018-12-03 20:02:13 +0800

    StickPin

---
+++ StickPin/Assets/Scripts/PinController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PinController : MonoBehaviour
+{
+    private bool isFly = false;
+    private bool isReach = false;
+    private Transform startPoint;
+    public float speed = 15f;
+
+    private Transform circle;
+
+    private Vector3 targetPosition; //距离圆周的向量
+
+
+    // Use this for initialization
+    void Start()
+    {
+        startPoint = GameObject.Find("StartPoint").transform;
+        circle = GameObject.Find("Circle").transform;
+
+        targetPosition.y = circle.position.y - 1.541f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*针就位*/
+        if (isFly == false)
+        {
+            if (isReach == false)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, startPoint.position, speed * Time.deltaTime);
+
+                if (Vector3.Distance(transform.position, startPoint.position) < 0.05f)
+                {
+                    isReach = true;
+                }
+            }
+        }
+
+        else
+        {
+            /*针朝圆发射*/
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, targetPosition) < 0.05f)
+            {
+                transform.position = targetPosition;
+
+                transform.parent = circle;
+
+                isFly = false;
+            }
+        }
+    }
+
+    public void StartFly()
+    {
+        isFly = true;
+        isReach = true;
+    }
+
+}
---
+++ StickPin/Assets/Scripts/PinHeadController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinHeadController : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "PinHead")
+        {
+            GameObject.Find("PinManager").GetComponent<PinManager>().GameOver();
+        }
+    }
+}
